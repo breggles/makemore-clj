efc66cbf65c446f67881475cb4f417f6596cf755
breggles <tilman.bregler@red-gate.com>
2023-12-08 17:27:07 +0000

    N

--- src/main.clj
+++ src/main.clj
@@ -1,15 +1,42 @@
 (ns main
   (:require [clojure.string :as string]
             [clojure.math :as math]
+            [clojure.pprint :as pprint]
             [clojure.inspector :as insp]))
 
-(def words (->> (slurp "./resources/names.txt")
-                (#(string/split % #"\n"))))
+(defonce words (->> (slurp "./resources/names.txt")
+                    (#(string/split % #"\n"))))
 
 (defn bigrams [word]
-  (let [chs (conj (vec (cons "<S>" word)) "<E>")]
-    (map vector chs (drop 1 chs))))
+  (let [chs (str \. word \.)]
+    (mapv vector chs (drop 1 chs))))
 
-(->> words
-     (take 2)
-     (mapcat bigrams))
+(defonce bigram->count (-> (mapcat bigrams words)
+                           (frequencies)))
+
+(defonce characters (set (string/join words)))
+
+(defonce c->i (-> (into {} (mapv vector characters (iterate inc 1)))
+                  (assoc \. 0)))
+
+(defn assoc-counts [N [[c1 c2] cnt]]
+  (assoc-in N [(c->i c1) (c->i c2)] cnt))
+
+(defonce N (let [char-count (count c->i)
+                 zeroes (->> (repeat char-count 0)
+                             (vec)
+                             (repeat char-count)
+                             (vec))]
+             (reduce assoc-counts zeroes bigram->count)))
+
+
+(comment
+
+  (defn neg [n] (* -1 n))
+
+  (sort-by (comp neg last) bigram->count)
+
+  (let [sum (apply + (nth N 0))]
+    (mapv #(/ % sum) (nth N 0)))
+
+  )
