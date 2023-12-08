d62975766b1b5796b70de6a127e3e3f4918eb22d
breggles <tilman.bregler@red-gate.com>
2023-12-08 12:32:02 +0000

    bigrams

--- src/main.clj
+++ src/main.clj
@@ -1,3 +1,15 @@
 (ns main
-  (:require [clojure.math :as math]
+  (:require [clojure.string :as string]
+            [clojure.math :as math]
             [clojure.inspector :as insp]))
+
+(def words (->> (slurp "./resources/names.txt")
+                (#(string/split % #"\n"))))
+
+(defn bigrams [word]
+  (let [chs (conj (vec (cons "<S>" word)) "<E>")]
+    (map vector chs (drop 1 chs))))
+
+(->> words
+     (take 2)
+     (mapcat bigrams))
